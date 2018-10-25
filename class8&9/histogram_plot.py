9fb68226e5869daeb5927aa872bfb09bbdb490aa
[fengyihuai] <[fengyihuai@outlook.com]>
2018-10-25 22:34:54 +0800

    edit histogram_plot.py

---
+++ class8&9/histogram_plot.py
@@ -0,0 +1,33 @@
+# -*- coding:UTF-8 -*-
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def file_name(file_dir):
+    L = []
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1] == '.jpg':
+                L.append(os.path.join(root, file))
+        if dirs or (not L):
+            for index in range(len(dirs)):
+                file_name(os.path.join(root, dirs))
+    return L
+
+a = file_name('images/自然风光/天空')
+for index in range(len(a)):
+    plt.figure(index + 1)
+    src = Image.open(a[index])
+    r, g, b = src.split()
+    ar = np.array(r).flatten()
+    plt.hist(ar, bins = 256, density=1, facecolor='r')
+    ag = np.array(g).flatten()
+    plt.hist(ag, bins=256, density=1, facecolor='g')
+    ab = np.array(b).flatten()
+    plt.hist(ab, bins=256, density=1, facecolor='b')
+    plt.show()
+print()
+# for i in range(len(a)):
+#     src =
