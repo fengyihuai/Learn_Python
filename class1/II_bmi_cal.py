d9e22e1c72ce926384484be14eff4956189d278c
fengyihuai <2963789080@qq.com>
2018-10-31 22:33:12 +0800

    rebase class 1

--- class1/II_bmi_cal.py
+++ class1/II_bmi_cal.py
@@ -5,19 +5,19 @@ name = input()
 height, weight = eval(input('请输入身高（厘米）和体重（千克）[用逗号隔开]：'))
 BMI = weight/((0.01*height)**2)
 print('您的BMI指数为:{:.2f}'.format(BMI))
-nat, dom='',''
+nat, dom='' ,''
 if BMI<18.5:
-    nat,dom='偏瘦','偏瘦'
+    nat, dom='偏瘦', '偏瘦'
 elif 18.5<=BMI<24:
     nat, dom = '正常', '正常'
 elif 24<=BMI<25:
-    nat,dom = '正常','偏胖'
+    nat, dom = '正常', '偏胖'
 elif 25<=BMI<28:
-    nat,dom = '偏胖','偏胖'
+    nat, dom = '偏胖', '偏胖'
 elif 28<=BMI<30:
-    nat, dom = '偏胖','肥胖'
+    nat, dom = '偏胖', '肥胖'
 else:
-    nat, dom = '肥胖','肥胖'
+    nat, dom = '肥胖', '肥胖'
 print('您的国际BMI指标是{0},国内BMI指标是{1}'.format(nat,dom))
 
 print()
--- class1/I_sum_odd.py
+++ class1/I_sum_odd.py
@@ -1,11 +1,20 @@
 # -*- coding: UTF-8 -*-
 # 1.计算从1到1000以内所有奇数的和并输出
+# solution 1
 sum = 0
 for num in range(1,1001):
     # print(num)
     if num%2:
         sum += num
 
+print('\nThe sum of odd integers ranging from 1 to 1000 is:', sum)
+
+# solution 2
+sum = 0
+n = 1000 - 1
+while n > 0:
+    sum = sum + n
+    n = n - 2
 print('\nThe sum of odd integers ranging from 1 to 1000 is:', sum)
 
 print()
