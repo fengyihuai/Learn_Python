612bcc52092ec71a37750233d85ad31380a614e9
fengyihuai <2963789080@qq.com>
2018-11-01 23:13:38 +0800

    class 4&5 - Code optimization no.3

--- class4to5/III_GBtoFasta.py
+++ class4to5/III_GBtoFasta.py
@@ -1,5 +1,6 @@
 # -*- coding:UTF-8 -*-
-
+# 3、从GenBank格式文件中提取登记码ACCESSION ，
+# 并将核苷酸序列写入一个Fasta格式的新文件中；
 f = open('AY810830.gb', 'r')
 # check = f.readlines()
 mark = "ACCESSION"
@@ -9,18 +10,20 @@ dna_seq = list()
 for line in f:
     if flag == False:
         if mark in line:
-            ID = line[len(mark):]
-            ID.replace('\n', '')
-            ID = ''.join(ID.split())
+            # ID = line[len(mark):]
+            # ID.replace('\n', '')
+            # ID = ''.join(ID.split())
+            ID = line.split()[1]
             ID = '>sp|'+ID+'|'
         elif DNA_M in line:
             flag = True
-            continue
     elif line[0:1] != '\\' and flag == True:
-        dna_str = line[10:65].upper()
-        dna_str = ''.join(dna_str.split())
-        dna_str.upper()
-        dna_seq.append(dna_str)
+        # dna_str = line[10:65].upper()
+        # merge the list to str
+        # dna_str = ''.join(dna_str.split())
+        dna_str = ''.join(line.split()[1:])
+        # Return a copy of str converted to uppercase
+        dna_seq.append(dna_str.upper())
 
 # Open for 'w'riting
 f = open('output.fasta', 'w')
