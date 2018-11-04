6b0ae6c35118612cfb7222333b2aa3946cd3c682
fengyihuai <2963789080@qq.com>
2018-11-04 20:51:49 +0800

    arxiv_crawler代码执行速度优化

--- course_project/arxiv_crawler.py
+++ course_project/arxiv_crawler.py
@@ -4,52 +4,68 @@
 本专业关键词1（例如：unet），搜索条件是，最新发表时间
 排序，获得50个数据。将搜索结果中的期刊地址（url链接）
 打印到命令行中
-附加要求(二选一)：...
+附加要求(二选一)：1 按上述步骤获取关键词2（例如：deeplab）的搜索结果
+，将前100个关键词1（unet）的搜索结果和前100个关键词2（deeplab）的搜索结果
+分别保存到两个txt文件中，再次读取文件后，使用正则表达式找到两部分搜索结果中
+重合的文献地址。代码需要输出准确运行时间，正则匹配越快，作业完成度越高。
+2 获取关键词1（unet）搜索页面中前三篇文献的完整描述。包含不限于：名称，
+作者，概述，全文等。内容越完整，作业完成度越高。
 '''
 
 import re
 import requests
-import urllib
+import time
 
+# 基于arxiv网站的关键词与显示检索条目可控函数，最终返回对应的
+# 文献期刊地址并可将其网址输出到命令行（可选项）
 def key_size_search(keyword, size, printed=False, print_num=0):
     try:
+        # arxiv网站的高级检索网站地址
         url = "https://arxiv.org/search/advanced?advanced="
-        payload = {"terms-0-operator": "AND", "terms-0-term": "python", \
+        # 高级检索参数，包括每页显示条数size，和检索词terms-0-term，构造传递的url参数
+        payload = {"terms-0-term": "python", \
                    "terms-0-field": "all", "size": "50"}
-
+        # 注：网站默认选择按最新发表时间显示文献条目，无需
+        # 再度指定"order":"-announced_date_first"
+        # payload = {"terms-0-term": "python", "terms-0-field": "all",\
+        #            "size": "50", "order":"-announced_date_first"}
+        # 输入检索词
         payload["terms-0-term"] = keyword
         if size in [20, 50, 100, 200]:
-
+            # 输入每页显示条数，仅限20，50，100，200-网站设定
             payload["size"] = str(size)
-
+            # 发送请求，尝试获取对应url网址的网页，返回Response对象
             r = requests.get(url, params=payload)
             print(r.status_code, r.url)
+            # 使用正则表达式找出文献期刊地址
             ar_address = re.compile(r"<a href=\"(.*)\">arXiv:[0-9\.]+</a>")
             address_url = ar_address.findall(r.text)
 
+            # printed为bool变量，为真，打印print_num条检索结果
             if printed:
                 print("\nkeyword:{}, size:{}, address:".format(keyword, len(address_url)))
+                # pirnt_num为输入或输入为0，以检索条目数为打印数
                 if print_num == 0:
-                    print_num = len(address_url)
+                    print_num = size
                 # for i in range(print_num):
                 #     print(address_url[i])
+                # 下面的代买执行速度是上面的10倍
                 print('\n'.join(address_url[:print_num]))
-
+            # 返回检索到的文献期刊地址
             return address_url
         else:
-            print("itmes' size must be 20, 50, 100 or 200")
+            print("itmes' size must be 20, 50, 100 or 200!!!")
 
     except:
         return ""
 
-# 1 按上述步骤获取关键词2（例如：deeplab）的搜索结果
-# ，将前100个关键词1（unet）的搜索结果和前100个关键词2（deeplab）的搜索结果
-# 分别保存到两个txt文件中，再次读取文件后，使用正则表达式找到两部分搜索结果中
-# 重合的文献地址。代码需要输出准确运行时间，正则匹配越快，作业完成度越高。
 def url_save_txt(file_name, url_list):
+    # 以“写”方式打开文件"xxx.txt",xxx为检索词，没有就新建
     f = open(file_name+'.txt', 'w')
-    for i in range(len(url_list)):
-        f.write("%s\n" % url_list[i])
+    # 写文件
+    for i_url in url_list:
+        f.write("%s\n" % i_url)
+    # 关闭文件
     f.close()
     print("%s.txt file is saved\n" % (file_name + ".txt"))
 
@@ -63,9 +79,6 @@ def read_match_urltxt(file_name1, file_name2):
             equalurl_list.append(url_list1[i])
     return equalurl_list
 
-
-# 2 获取关键词1（unet）搜索页面中前三篇文献的完整描述。包含不限于：名称，
-# 作者，概述，全文等。内容越完整，作业完成度越高。
 def arxiv_description(url):
     try:
         r = requests.get(url)
@@ -96,6 +109,9 @@ def arxiv_description(url):
         return ""
 
 def main():
+
+    start = time.clock()
+
     uid = ["electroencephalogram", "event-related potential"]
 
     k1s2_adurl = key_size_search(uid[0], 100, printed=True, print_num=50)
@@ -107,9 +123,15 @@ def main():
     same_urllist = read_match_urltxt("electroencephalogram.txt", "event-related potential.txt")
     print("the same urls are:\n{}".format('\n'.join(same_urllist)))
 
-    # k1s2_adurl = key_size_search(uid[0], 100, printed=True, print_num=50)
+    end = time.clock()
+
     for i in range(3):
         arxiv_description(k1s2_adurl[i])
+
+    # 输出问题1涉及功能代码运行时间
+    print("----------------------------------------------------")
+    print("\nRunning time for problem 1 is:", end-start)
+    print("----------------------------------------------------")
 
 if __name__ == '__main__':
     main()
